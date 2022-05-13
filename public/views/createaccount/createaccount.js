c2c74647193b310c28feb926e0dc8459ceca2462
Ulvy2022 <ulvy.romy@student.passerellesnumeriques.org>
2022-05-13 09:36:02 +0700

    fixs

--- public/views/createaccount/createaccount.js
+++ public/views/createaccount/createaccount.js
@@ -1,18 +1,22 @@
 // add tasks to the database
 function addTask(e) {
     // e.preventDefault();
-    let url = "/addUser";
+    let url = "/addUser/";
     var username = document.getElementById("username").value;
     var psw = document.getElementById("password").value;
     var email = document.getElementById("emailAddress").value;
     var birthday = document.getElementById("birthdayDate").value;
     var gender = "Male";
+    let getUser = "/dataUsers/" + psw + "/" + username;
     if (document.getElementById("femaleGender").checked) {
         gender = "Female";
     }
     // TODO: request the server to create new task
     axios.post(url, { username: username, password: psw, email: email, birthday: birthday, gender: gender }).then(() => {
-        location.href = "../homepage/home.html";
+        axios.get(getUser).then((response) => {
+            localStorage.setItem("userID", response.data._id)
+            location.href = "../homepage/home.html";
+        })
     })
 }
 
